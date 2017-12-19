0fe6478b3923a31a46a0e75d40647ef0749a67b1
AchmadSetiaji <achmad.setiaji@moonlay.com>
2017-12-19 14:01:10 +0700

    update to minimize using

--- Com.Bateeq.Service.Core.Lib/Services/ArticleCategoryService.cs
+++ Com.Bateeq.Service.Core.Lib/Services/ArticleCategoryService.cs
@@ -1,9 +1,6 @@
 ﻿using Com.Bateeq.Service.Core.Lib.Context;
 using Com.Bateeq.Service.Core.Lib.Models.Article;
-using Com.Moonlay.NetCore.Lib.Service;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Com.Bateeq.Service.Core.Lib.Services
 {
