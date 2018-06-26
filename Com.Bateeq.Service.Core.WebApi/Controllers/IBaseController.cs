eadff54e6627ab4f1f125da8270a53cb97282d0e
AchmadSetiaji <achmad.setiaji@moonlay.com>
2018-06-26 18:30:53 +0700

    update

--- Com.Bateeq.Service.Core.WebApi/Controllers/IBaseController.cs
+++ Com.Bateeq.Service.Core.WebApi/Controllers/IBaseController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Com.Bateeq.Service.Core.WebApi.Controllers
 {
     public interface IBaseController<TViewModel>
     {
+        IActionResult Get(int Page = 1, int Size = 25, string Order = "{}", [Bind(Prefix = "Select[]")]List<string> Select = null, string Keyword = null, string Filter = "{}");
         Task<IActionResult> GetByUId([FromRoute] string id);
         Task<IActionResult> GetById([FromRoute] int id);
         Task<ActionResult> Post([FromBody] TViewModel viewModel);
