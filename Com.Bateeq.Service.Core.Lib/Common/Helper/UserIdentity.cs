a9b1a180fd27f5e3a7f7901c188a4372a001e7b3
Andi Budi <andibateeq@gmail.com>
2019-09-24 09:15:15 +0700

    remove old

--- Com.Bateeq.Service.Core.Lib/Common/Helper/UserIdentity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Com.Bateeq.Service.Core.Lib.Common.Helper
-{
-    public class UserIdentity
-    {
-        public string Username { get; set; }
-        public string Token { get; set; }
-    }
-}
