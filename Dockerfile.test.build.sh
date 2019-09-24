d56b3729ebc907fa53be7703c6fa64cb8b25c744
Andi Budi <andibateeq@gmail.com>
2019-09-24 11:34:30 +0700

    Update Dockerfile.test.build.sh
    repository name must be lowercase

--- Dockerfile.test.build.sh
+++ Dockerfile.test.build.sh
@@ -1,5 +1,5 @@
-docker build -f Dockerfile.test.build -t com-Bateeq-service-core-webapi:test-build .
-docker create --name com-Bateeq-service-core-webapi-test-build com-Bateeq-service-core-webapi:test-build
+docker build -f Dockerfile.test.build -t com-bateeq-service-core-webapi:test-build .
+docker create --name com-bateeq-service-core-webapi-test-build com-bateeq-service-core-webapi:test-build
 mkdir bin
-docker cp com-Bateeq-service-core-webapi-test-build:/out/. ./bin/publish
-docker build -f Dockerfile.test -t com-Bateeq-service-core-webapi:test .
+docker cp com-bateeq-service-core-webapi-test-build:/out/. ./bin/publish
+docker build -f Dockerfile.test -t com-bateeq-service-core-webapi:test .
