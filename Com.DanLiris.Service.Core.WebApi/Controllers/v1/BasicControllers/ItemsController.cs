22fd0eb5aaec148865d3b8eba74e07f0dc7b7fae
Fadhlinp <fadhli.nopri.pratama@gmail.com>
2021-08-04 06:40:33 +0700

    Insert New Items

--- Com.DanLiris.Service.Core.WebApi/Controllers/v1/BasicControllers/ItemsController.cs
+++ Com.DanLiris.Service.Core.WebApi/Controllers/v1/BasicControllers/ItemsController.cs
@@ -310,5 +310,38 @@ namespace Com.DanLiris.Service.Core.WebApi.Controllers.v1.BasicControllers
             }
         }
 
+        [HttpPost("item")]
+        public async Task<IActionResult> PostItem([FromBody] ItemViewModelUsername ViewModel)
+        {
+            try
+            {
+                Item model = Service.ItemMapToModel(ViewModel);
+
+                Service.Username = ViewModel.Username;
+                Service.Token = ViewModel.Token;
+
+                await Service.CreateModel(model);
+
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.CREATED_STATUS_CODE, General.OK_MESSAGE)
+                    .Ok();
+                return Created(String.Concat(HttpContext.Request.Path, "/", model.Id), Result);
+            }
+            catch (ServiceValidationExeption e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.BAD_REQUEST_STATUS_CODE, General.BAD_REQUEST_MESSAGE)
+                    .Fail(e);
+                return BadRequest(Result);
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
+
     }
 }
